e37ce18c64472ac469b2ec8146af75d7d64f1a06
echiou <ethanchiou@berkeley.edu>
2014-03-09 09:26:27 -0700

    FINAL?

--- keypresses.js
+++ keypresses.js
@@ -1,50 +1,55 @@
 $(document).ready(function(){
     var angle = 0;
     
-    var back = new Array();
-    var funcArray = new Array();
-    var dispArray= new Array();
-    var menuFuncArray = new Array();
-    var audioFuncArray = new Array();
-    
-    menuFuncArray[0] = audioFunc;
-    audioFuncArray[99] = auxFunc;
-    audioFuncArray[111] = btFunc;
-    audioFuncArray[119] = fmFunc;
-    audioFuncArray[109] = amFunc;
     
     
     
     // AUXXXXXXXXXXXXXXXXXXXXX
     function auxFunc() {
-        $("#nwlabel").css("color", "red");
+        $("#nwlabel").css("color", "#f13c00");
         $("#nelabel").css("color", "white");
         $("#selabel").css("color", "white");
         $("#swlabel").css("color", "white");
     }
     function btFunc() {
-        $("#nelabel").css("color", "red");
+        $("#nelabel").css("color", "#f13c00");
         $("#nwlabel").css("color", "white");
         $("#selabel").css("color", "white");
         $("#swlabel").css("color", "white");
     }
     function fmFunc() {
-        $("#swlabel").css("color", "red");
+        $("#swlabel").css("color", "#f13c00");
         $("#nelabel").css("color", "white");
         $("#selabel").css("color", "white");
         $("#nwlabel").css("color", "white");
     }
     function amFunc() {
-        $("#selabel").css("color", "red");
+        $("#selabel").css("color", "#f13c00");
         $("#nelabel").css("color", "white");
         $("#nwlabel").css("color", "white");
         $("#swlabel").css("color", "white");
     }
+    // BACKKKKKKKKKKKKKKKKKKKKKKKKK
+    function backFunc() {
+        $("#selabel").css("color", "white");
+        $("#nelabel").css("color", "white");
+        $("#nwlabel").css("color", "white");
+        $("#swlabel").css("color", "white");
+        $("#nwlabel").text("AUDIO\xa0");
+        $("#nelabel").text("\xa0\xa0\xa0A/C");
+        $("#swlabel").text("BT\xa0\xa0");
+        $("#selabel").text("\xa0\xa0CONFIG");
+        menuFunc();
+    }
     function audioFunc() {
-        $("#nwlabel").text("AUX\xa0\xa0\xa0\xa0");
-        $("#nelabel").text("\xa0\xa0\xa0\xa0BT");
-        $("#swlabel").text("AM\xa0\xa0\xa0\xa0");
-        $("#selabel").text("\xa0\xa0\xa0\xa0FM");
+        angle = 0;
+        $("#dial").rotate(angle);
+        $("#reverseDial").rotate(-angle);
+        $("#nwlabel").text("AUX\xa0\xa0\xa0");
+        $("#nelabel").text("\xa0\xa0\xa0BT");
+        $("#swlabel").text("AM\xa0\xa0\xa0");
+        $("#selabel").text("\xa0\xa0\xa0FM");
+        $("#reverseDial").html(init);
         $(document).keypress(function(e){
             if (e.keyCode == 119) {
                 fmFunc();
@@ -58,36 +63,51 @@ $(document).ready(function(){
             else if (e.keyCode == 109) {
                 amFunc();
             }
+            else if (e.keyCode == 98) {
+                backFunc();
+            }
         });
     }
-    $(document).keypress(function(e){
+    function menuFunc() {
+        $(document).keypress(function(e){
         if (e.keyCode == 119) {
+            init = 15;
             audioFunc();
         }
         else if (e.keyCode == 99) {
+            init = 15;
             audioFunc();
         }
         else if (e.keyCode == 111) {
+            init = 15;
             audioFunc();
         }
         else if (e.keyCode == 109) {
+            init = 15;
             audioFunc();
         }
    
-        else if (e.keyCode == 103) { 
+        else if (e.keyCode == 104) { 
             angle -= 40;
             $("#dial").rotate(angle);
+            $("#reverseDial").rotate(-angle);
+            init -= 2;
+            $("#reverseDial").html(init);
         }
-        else if (e.keyCode == 104) { 
+        else if (e.keyCode == 103) { 
             angle += 40;
             $("#dial").rotate(angle);
+            $("#reverseDial").rotate(-angle);
+            init += 2;
+            $("#reverseDial").html(init);
         }
     });
+    }
     
-    // BACKKKKKKKKKKKKKKKKKKKKKKKKK
-    $(document).keypress(function(e){
-        if (e.keyCode == 119) {
-            audioFunc();
-        }
-    });
+    var init = 65;
+    $("#reverseDial").html(init);
+    menuFunc();
+
+    
+
 });

e229ac3cef2bf51750c6975747a718b6ca7205e2
Jonathan Wang <jonathanwang017@berkeley.edu>
2014-03-08 17:56:52 -0800

    button = light - it works!

--- consl/consl.ino
+++ consl/consl.ino
@@ -1,47 +1,27 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
+
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-int led = 13;
-int motorLeft = 1;
-int motorRight = 3;
+const int ledPin = 13;
+const int buttonPin = 2;
+
+int buttonState = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);    
-  pinMode(motorLeft, OUTPUT); 
-  pinMode(motorRight, OUTPUT);
+  pinMode(ledPin, OUTPUT);    
+  pinMode(buttonPin, INPUT); 
 }
 
-void turnRight() {
-  digitalWrite(motorLeft, HIGH);
-  delay(1000);
-  digitalWrite(motorLeft, LOW);
-}
 
-void turnLeft() {
-  digitalWrite(motorRight, HIGH);
-  delay(1000);
-  digitalWrite(motorRight, LOW);
-}
-
-void moveForward() {
-  digitalWrite(motorRight, HIGH);
-  digitalWrite(motorLeft, HIGH);
-  delay(1000);
-  digitalWrite(motorRight, LOW);
-  digitalWrite(motorLeft, LOW);
-}
   
 // the loop routine runs over and over again forever:
 void loop() {
-  turnLeft();
-  turnRight();
-  moveForward();
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
 }

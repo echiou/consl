e3f4e1a307d719c2cc5b436f48ff7f04fb48226b
Jonathan Wang <jonathanwang017@berkeley.edu>
2014-03-10 23:19:47 -0700

    fix js file
    organize menu structure, fix buggy parts

--- keypresses.js
+++ keypresses.js
@@ -1,48 +1,81 @@
-$(document).ready(function(){
+$(document).ready(function() {
     var angle = 0;
+    var init = 65;
+    var savedTemp;    
+    var currentMenu = "main";
+
+    $(document).keypress(function(e) {
+        if (currentMenu == "main") {
+            menuFunc(e.keyCode);
+        }
+        else if (currentMenu == "audio") {
+            audioFunc(e.keyCode);
+        }
+        else if (currentMenu == "ac") {
+            acFunc(e.keyCode);   
+        }
+        else {
+            alert("uhoh");
+        }
+    });
     
-    
-    
+    function nwFunc() {
+        $("#nelabel").css("color", "white");
+        $("#nwlabel").css("color", "red");
+        $("#selabel").css("color", "white");
+        $("#swlabel").css("color", "white");
+    }
+    function neFunc() {
+        $("#nelabel").css("color", "red");
+        $("#nwlabel").css("color", "white");
+        $("#selabel").css("color", "white");
+        $("#swlabel").css("color", "white");
+    }
+    function swFunc() {
+        $("#nelabel").css("color", "white");
+        $("#nwlabel").css("color", "white");
+        $("#selabel").css("color", "white");
+        $("#swlabel").css("color", "red");
+    }
+    function seFunc() {
+        $("#nelabel").css("color", "white");
+        $("#nwlabel").css("color", "white");
+        $("#selabel").css("color", "red");
+        $("#swlabel").css("color", "white");
+    }
     
     // AUXXXXXXXXXXXXXXXXXXXXX
     function auxFunc() {
-        $("#nwlabel").css("color", "#f13c00");
-        $("#nelabel").css("color", "white");
-        $("#selabel").css("color", "white");
-        $("#swlabel").css("color", "white");
+        nwFunc();
     }
     function btFunc() {
-        $("#nelabel").css("color", "#f13c00");
-        $("#nwlabel").css("color", "white");
-        $("#selabel").css("color", "white");
-        $("#swlabel").css("color", "white");
+        neFunc();
     }
     function fmFunc() {
-        $("#swlabel").css("color", "#f13c00");
-        $("#nelabel").css("color", "white");
-        $("#selabel").css("color", "white");
-        $("#nwlabel").css("color", "white");
+        seFunc();
     }
     function amFunc() {
-        $("#selabel").css("color", "#f13c00");
-        $("#nelabel").css("color", "white");
-        $("#nwlabel").css("color", "white");
-        $("#swlabel").css("color", "white");
+        swFunc();
     }
+    function airFunc() {
+        nwFunc();
+    }
+    function circFunc() {
+        neFunc();
+    }
+    function rdefrostFunc() {
+        swFunc();
+    }
+    function fdefrostFunc() {
+        seFunc();
+    }
+    
     // BACKKKKKKKKKKKKKKKKKKKKKKKKK
     function backFunc() {
-        $("#selabel").css("color", "white");
-        $("#nelabel").css("color", "white");
-        $("#nwlabel").css("color", "white");
-        $("#swlabel").css("color", "white");
-        $("#nwlabel").text("AUDIO\xa0");
-        $("#nelabel").text("\xa0\xa0\xa0A/C");
-        $("#swlabel").text("BT\xa0\xa0");
-        $("#selabel").text("\xa0\xa0CONFIG");
+        currentMenu = "main";
         menuFunc();
-        $("#reverseDial").html("71");
     }
-    function audioFunc() {
+    function audioFunc(code) {
         angle = 0;
         $("#dial").rotate(angle);
         $("#reverseDial").rotate(-angle);
@@ -51,70 +84,93 @@ $(document).ready(function(){
         $("#swlabel").text("AM\xa0\xa0\xa0");
         $("#selabel").text("\xa0\xa0\xa0FM");
         $("#reverseDial").html(init);
-        $(document).keypress(function(e){
-            if (e.keyCode == 119) {
-                fmFunc();
-            }
-            else if (e.keyCode == 99) {
-                auxFunc();
-            }
-            else if (e.keyCode == 111) { 
-                btFunc();
-            }
-            else if (e.keyCode == 109) {
-                amFunc();
-            }
-            else if (e.keyCode == 98) {
-                backFunc();
-            }
-        });
+        if (code == 119) { //W
+            auxFunc();
+        }
+        else if (code == 99) { //C
+            amFunc();
+        }
+        else if (code == 111) { //O
+            btFunc();
+        }
+        else if (code == 109) { //M
+            fmFunc();
+        }
+        else if (code == 98) {
+            backFunc();
+        }
     }
-    function menuFunc() {
-        $(document).keypress(function(e){
-        if (e.keyCode == 119) {
+    function acFunc(code) {
+        angle = 0;
+        $("#dial").rotate(angle);
+        $("#reverseDial").rotate(-angle);
+        $("#nwlabel").text("AIR\xa0\xa0\xa0");
+        $("#nelabel").text("\xa0\CIRC");
+        $("#swlabel").text("REAR\xa0");
+        $("#selabel").text("\xa0FRONT");
+        $("#reverseDial").html(init);
+        if (code == 119) { //W
+            airFunc();
+        }
+        else if (code == 99) { //C
+            rdefrostFunc();
+        }
+        else if (code == 111) {  //O
+            circFunc();
+        }
+        else if (code == 109) { //M
+            fdefrostFunc();
+        }
+        else if (code == 98) {
+            backFunc();
+        }
+    }
+    function menuFunc(code) {
+        $("#selabel").css("color", "white");
+        $("#nelabel").css("color", "white");
+        $("#nwlabel").css("color", "white");
+        $("#swlabel").css("color", "white");
+        $("#nwlabel").text("AUDIO\xa0");
+        $("#nelabel").text("\xa0\xa0\xa0A/C");
+        $("#swlabel").text("BT\xa0\xa0");
+        $("#selabel").text("\xa0\xa0CONFIG");
+        if (code == 119) { //W
             savedTemp = init;
             init = 15;
+            currentMenu = "audio";
             audioFunc();
         }
-        else if (e.keyCode == 99) {
+        else if (code == 99) { //C
             savedTemp = init;
             init = 15;
+            currentMenu = "audio";
             audioFunc();
         }
-        else if (e.keyCode == 111) {
+        else if (code == 111) { //O
             savedTemp = init;
             init = 15;
-            audioFunc();
+            currentMenu = "ac";
+            acFunc();
         }
-        else if (e.keyCode == 109) {
+        else if (code == 109) { //M
             savedTemp = init;
             init = 15;
+            currentMenu = "audio";
             audioFunc();
         }
-   
-        else if (e.keyCode == 104) { 
+        else if (code == 104) { 
             angle -= 40;
             $("#dial").rotate(angle);
             $("#reverseDial").rotate(-angle);
             init -= 2;
             $("#reverseDial").html(init);
         }
-        else if (e.keyCode == 103) { 
+        else if (code == 103) { 
             angle += 40;
             $("#dial").rotate(angle);
             $("#reverseDial").rotate(-angle);
             init += 2;
             $("#reverseDial").html(init);
         }
-    });
     }
-    
-    var init = 65;
-    var savedTemp;
-    
-    $("#reverseDial").html(init);
-    menuFunc();
-
-    
-
 });

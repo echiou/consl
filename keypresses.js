88b39c34eb62e7fa9be19964931de9b70345d731
Jonathan Wang <jonathanwang017@berkeley.edu>
2014-03-11 00:02:46 -0700

    fix turning problems
    edit the dial mechanic to store all variables and display dial position
    based on selected button

--- keypresses.js
+++ keypresses.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
     var angle = 0;
     var init = 65;
-    var savedTemp;    
+    var vol = 3;
+    var aux = 6;
+    var am = 2;
+    var fm = 4;
+    var temp = 8;
+    var air = 9;
+    var rdefrost = 10;
+    var fdefrost = 20;
+    
+    var dialType = null;
     var currentMenu = "main";
+    
 
     $(document).keypress(function(e) {
         if (currentMenu == "main") {
@@ -45,53 +55,172 @@ $(document).ready(function() {
     }
     
     // AUXXXXXXXXXXXXXXXXXXXXX
-    function auxFunc() {
+    function volFunc() {
+        dialType = "vol";
         nwFunc();
+        checkDial();
     }
-    function btFunc() {
+    function auxFunc() {
+        dialType = "aux";
         neFunc();
+        checkDial();
     }
     function fmFunc() {
+        dialType = "fm";
         seFunc();
+        checkDial();
     }
     function amFunc() {
+        dialType = "am";
         swFunc();
+        checkDial();
+    }
+    function tempFunc() {
+        dialType = "temp";
+        nwFunc();
+        checkDial();
     }
     function airFunc() {
-        nwFunc();
-    }
-    function circFunc() {
+        dialType = "air";
         neFunc();
+        checkDial();
     }
     function rdefrostFunc() {
+        dialType = "rdefrost";
         swFunc();
+        checkDial();
     }
     function fdefrostFunc() {
+        dialType = "fdefrost";
         seFunc();
+        checkDial();
     }
     
     // BACKKKKKKKKKKKKKKKKKKKKKKKKK
     function backFunc() {
         currentMenu = "main";
+        dialType = null;
+        checkDial();
         menuFunc();
     }
-    function audioFunc(code) {
-        angle = 0;
+    
+    function checkDial() {
+        switch(dialType) {
+            case "vol":
+                $("#reverseDial").text(vol);            
+                break;
+            case "aux":
+                $("#reverseDial").text(aux);
+                break;
+            case "am":
+                $("#reverseDial").text(am);
+                break;
+            case "fm":
+                $("#reverseDial").text(fm);
+                break;
+            case "temp":
+                $("#reverseDial").text(temp);            
+                break;
+            case "air":
+                $("#reverseDial").text(air);
+                break;
+            case "rdefrost":
+                $("#reverseDial").text(rdefrost);
+                break;
+            case "fdefrost":
+                $("#reverseDial").text(fdefrost);
+                break; 
+            default:
+                $("#reverseDial").text("");
+                break;
+        }
+    }
+    function turnDialForward() {
+        angle += 40;
         $("#dial").rotate(angle);
         $("#reverseDial").rotate(-angle);
-        $("#nwlabel").text("AUX\xa0\xa0\xa0");
-        $("#nelabel").text("\xa0\xa0\xa0BT");
+        switch(dialType) {
+            case "vol":
+                vol += 2;            
+                break;
+            case "aux":
+                aux += 2;
+                break;
+            case "am":
+                am += 2;
+                break;
+            case "fm":
+                fm += 2;
+                break;
+            case "temp":
+                temp += 2;            
+                break;
+            case "air":
+                air += 2;
+                break;
+            case "rdefrost":
+                rdefrost += 2;
+                break;
+            case "fdefrost":
+                fdefrost += 2;
+                break; 
+            default:
+                break;
+        }
+        checkDial();
+    }
+    function turnDialBackward() {
+        angle -= 40;
+        $("#dial").rotate(angle);
+        $("#reverseDial").rotate(-angle);
+        switch(dialType) {
+            case "vol":
+                vol -= 2;            
+                break;
+            case "aux":
+                aux -= 2;
+                break;
+            case "am":
+                am -= 2;
+                break;
+            case "fm":
+                fm -= 2;
+                break;
+            case "temp":
+                temp -= 2;            
+                break;
+            case "air":
+                air -= 2;
+                break;
+            case "rdefrost":
+                rdefrost -= 2;
+                break;
+            case "fdefrost":
+                fdefrost -= 2;
+                break; 
+            default:
+                break;
+        }
+        checkDial();
+    }
+    function audioFunc(code) {
+        $("#dial").rotate(angle);
+        $("#reverseDial").rotate(-angle);
+        $("#nwlabel").text("VOL\xa0\xa0\xa0");
+        $("#nelabel").text("\xa0\xa0\AUX");
         $("#swlabel").text("AM\xa0\xa0\xa0");
         $("#selabel").text("\xa0\xa0\xa0FM");
-        $("#reverseDial").html(init);
+        checkDial();
+        
+        
         if (code == 119) { //W
-            auxFunc();
+            volFunc();
         }
         else if (code == 99) { //C
             amFunc();
         }
         else if (code == 111) { //O
-            btFunc();
+            auxFunc();
         }
         else if (code == 109) { //M
             fmFunc();
@@ -99,24 +228,30 @@ $(document).ready(function() {
         else if (code == 98) {
             backFunc();
         }
+        else if (code == 103) { 
+            turnDialBackward();
+        }
+        else if (code == 104) { 
+            turnDialForward();
+        }
     }
     function acFunc(code) {
-        angle = 0;
         $("#dial").rotate(angle);
         $("#reverseDial").rotate(-angle);
-        $("#nwlabel").text("AIR\xa0\xa0\xa0");
-        $("#nelabel").text("\xa0\CIRC");
+        $("#nwlabel").text("TEMP\xa0\xa0");
+        $("#nelabel").text("\xa0\xa0\AIR");
         $("#swlabel").text("REAR\xa0");
         $("#selabel").text("\xa0FRONT");
-        $("#reverseDial").html(init);
+        checkDial();
+        
         if (code == 119) { //W
-            airFunc();
+            tempFunc();
         }
         else if (code == 99) { //C
             rdefrostFunc();
         }
         else if (code == 111) {  //O
-            circFunc();
+            airFunc();
         }
         else if (code == 109) { //M
             fdefrostFunc();
@@ -124,53 +259,45 @@ $(document).ready(function() {
         else if (code == 98) {
             backFunc();
         }
+        else if (code == 103) { 
+            turnDialBackward();
+        }
+        else if (code == 104) { 
+            turnDialForward();
+        }
     }
     function menuFunc(code) {
         $("#selabel").css("color", "white");
         $("#nelabel").css("color", "white");
         $("#nwlabel").css("color", "white");
         $("#swlabel").css("color", "white");
-        $("#nwlabel").text("AUDIO\xa0");
+        $("#nwlabel").text("AUDIO\xa0\xa0");
         $("#nelabel").text("\xa0\xa0\xa0A/C");
         $("#swlabel").text("BT\xa0\xa0");
         $("#selabel").text("\xa0\xa0CONFIG");
         if (code == 119) { //W
-            savedTemp = init;
-            init = 15;
             currentMenu = "audio";
+            //dialType = "vol"
             audioFunc();
         }
         else if (code == 99) { //C
-            savedTemp = init;
-            init = 15;
             currentMenu = "audio";
             audioFunc();
         }
         else if (code == 111) { //O
-            savedTemp = init;
-            init = 15;
             currentMenu = "ac";
+            //dialType = "temp"
             acFunc();
         }
         else if (code == 109) { //M
-            savedTemp = init;
-            init = 15;
             currentMenu = "audio";
             audioFunc();
         }
-        else if (code == 104) { 
-            angle -= 40;
-            $("#dial").rotate(angle);
-            $("#reverseDial").rotate(-angle);
-            init -= 2;
-            $("#reverseDial").html(init);
-        }
         else if (code == 103) { 
-            angle += 40;
-            $("#dial").rotate(angle);
-            $("#reverseDial").rotate(-angle);
-            init += 2;
-            $("#reverseDial").html(init);
+            turnDialBackward();
+        }
+        else if (code == 104) { 
+            turnDialForward();
         }
     }
 });
